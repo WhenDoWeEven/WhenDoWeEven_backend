84e4aeadc0d0b6b29f8742e667d78260eafaab3b
viveksenthil3 <svivek.hosur5@gmail.com>
2025-02-22 16:51:11 -0500

    basic functionality

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userType: {
+        type: String,
+        required: true,
+        default: 'temp'
+    },
+    refresh_token: {
+        type: String,
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
