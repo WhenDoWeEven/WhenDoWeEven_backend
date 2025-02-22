3a4d8c5f0a77768239d96115f6cc14c35b26ed9d
Trust-Worthy <jondeveloper0@gmail.com>
2025-02-22 14:46:34 -0500

    Done creating file structure. Going to start with the calendar first.

--- whendoweeven/src/cal-parser/parser.py
+++ whendoweeven/src/cal-parser/parser.py
@@ -1,20 +1,19 @@
 import icalendar
 from typing import Optional, Any
+import json
 
-
-def get_type_file():
+def get_file_type(file:str):
     pass
-def parse_ics():
+def parse_ics(ics_file:str):
     pass
-def parse_ical():
+def parse_ical(ical_file):
     pass
-def parse_ifb():
+def parse_ifb(ifb_file):
     pass
 
 
-
-
-
+def main():
+    pass
 
 if __name__ == "__main__":
     main()
---
+++ whendoweeven/src/mongoDB/upload_data.py
@@ -0,0 +1,16 @@
+from typing import Any, Optional
+import json
+
+def send_data_to_mongoDB(json:json):
+    pass
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
