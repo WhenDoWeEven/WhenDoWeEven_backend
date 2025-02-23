8634e3a449692af8d6702bd5e0ece16e80dddd16
Trust-Worthy <jondeveloper0@gmail.com>
2025-02-22 22:33:23 -0500

    Updating gitignore.

--- whendoweeven/src/mongoDB/retrieve_data.py
+++ whendoweeven/src/mongoDB/retrieve_data.py
@@ -1,16 +1,22 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://vs9589:BRICKHACK11@cluster0.2gslk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+load_dotenv()
+PASSWORD: str = os.getenv("PASSWORD")
+uri = "mongodb+srv://vs9589:{PASSWORD}@cluster0.2gslk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+
+client.admin.command('ping')
+print("Pinged your deployment. You successfully connected to MongoDB!")
 
 
 
 def connect_to_DB():
+    pass
